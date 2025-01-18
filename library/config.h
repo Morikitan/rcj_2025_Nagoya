04721c2774d85848ea6c7701bb064ab38ad794f4
Morikitan <morikitan25@gmail.com>
2025-01-18 17:02:17 +0900

    MainMotorState作り途中

--- library/config.h
+++ library/config.h
@@ -1,4 +1,42 @@
 #pragma once
 
-#define MMpin1_1 1
 
+//MainMotor
+#define MMpin1_1 6
+#define MMpin1_2 7
+#define MMpin2_1 10
+#define MMpin2_2 11
+#define MMpin3_1 12
+#define MMpin3_2 13
+#define MMpin4_1 14
+#define MMpin4_2 15
+
+//DribblerMotor
+#define DMpin1 16
+#define DMpin2 17
+
+//UART
+#define TX0pin 0
+#define RX0pin 1
+#define TX1pin 8
+#define RX1pin 9
+
+//I2C
+#define SDA1pin 2
+#define SCL1pin 3
+#define SDA0pin 4
+#define SCL0pin 5
+
+//TactSwitch
+#define TSpin1 18
+#define TSpin2 19
+#define TSpin3 20
+#define TSpin4 21
+#define TSpin5 22
+#define TSpin6 26
+
+//Buzzer
+#define Bupin 27
+
+//DribblerSensor
+#define DSpin 28
--- library/motor/motor.c
+++ library/motor/motor.c
@@ -4,6 +4,82 @@
 #include "hardware/pwm.h"
 #include "../config.h"
 
-void MainMotorState(int motor,int state,int speed){
-
+void MainMotorState(int motor, int state, int speed) {
+  if (motor == 1) {
+    if (state == 0) {
+      analogWrite(MMpin1_1, speed);
+      analogWrite(MMpin1_2, 0);
+    } else if (state == 1) {
+      analogWrite(MMpin1_1, 0);
+      analogWrite(MMpin1_2, speed);
+    } else if (state == 2) {
+      analogWrite(MMpin1_1, 0);
+      analogWrite(MMpin1_2, 0);
+    } else if (state == 3) {
+      if(speed == 255){
+        gpio_put(MMpin1_1, 1);
+        gpio_put(MMpin1_2, 1);
+      }else{
+        analogWrite(MMpin1_1, speed);
+        analogWrite(MMpin1_2, speed);
+      }
+    }
+  } else if (motor == 2) {
+    if (state == 0) {
+      analogWrite(MMpin2_1, speed);
+      analogWrite(MMpin2_2, 0);
+    } else if (state == 1) {
+      analogWrite(MMpin2_1, 0);
+      analogWrite(MMpin2_2, speed);
+    } else if (state == 2) {
+      analogWrite(MMpin2_1, 0);
+      analogWrite(MMpin2_2, 0);
+    } else if (state == 3) {
+      if(speed == 255){
+        gpio_put(MMpin2_1, 1);
+        gpio_put(MMpin2_2, 1);
+      }else{
+        analogWrite(MMpin2_1, speed);
+        analogWrite(MMpin2_2, speed);
+      }
+    }
+  } else if (motor == 3) {
+    if (state == 0) {
+      analogWrite(MMpin3_1, speed);
+      analogWrite(MMpin3_2, 0);
+    } else if (state == 1) {
+      analogWrite(MMpin3_1, 0);
+      analogWrite(MMpin3_2, speed);
+    } else if (state == 2) {
+      analogWrite(MMpin3_1, 0);
+      analogWrite(MMpin3_2, 0);
+    } else if (state == 3) {
+      if(speed == 255){
+        gpio_put(MMpin3_1, 1);
+        gpio_put(MMpin3_2, 1);
+      }else{
+        analogWrite(MMpin3_1, speed);
+        analogWrite(MMpin3_2, speed);
+      }
+    }
+  } else if (motor == 4) {
+    if (state == 0) {
+      analogWrite(MMpin4_1, speed);
+      analogWrite(MMpin4_2, 0);
+    } else if (state == 1) {
+      analogWrite(MMpin4_1, 0);
+      analogWrite(MMpin4_2, speed);
+    } else if (state == 2) {
+      analogWrite(MMpin4_1, 0);
+      analogWrite(MMpin4_2, 0);
+    } else if (state == 3) {
+      if(speed == 255){
+        gpio_put(MMpin4_1, 1);
+        gpio_put(MMpin4_2, 1);
+      }else{
+        analogWrite(MMpin4_1, speed);
+        analogWrite(MMpin4_2, speed);
+      }
+    }
+  }
 }
