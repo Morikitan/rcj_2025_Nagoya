e2ec322c289cdbfb25cf6f6bb39f4de72c674093
Morikitan <morikitan25@gmail.com>
2025-01-20 21:31:38 +0900

    UART関数宣言されてないもんだ解決します

--- library/Nano33IoT/Nano33IoT.c
+++ library/Nano33IoT/Nano33IoT.c
@@ -17,11 +17,57 @@ void UseBallSensor(){
         if(kurikaesi > 100){
             kurikaesi = 0;
             uart_flush();
+            if(SerialWatch == 'b'){
+            printf("flushしました。\n");
+            }
         }
     }
-    uart_putc(uart1,0x01);  //BallSensorの値を要求
-    uint8_t received_char = uart_getc_timeout(uart0, 10); 
-    if(received_char == PICO_ERROR_TIMEOUT){
+    //BallSensorの値を要求
+    uart_putc(uart1,0x01); 
+    uart_putc(uart1,0x03); 
+    uint8_t data = uart_getc_timeout(uart1, 10); 
+    if(data == PICO_ERROR_TIMEOUT){
+        BallAngle = -999;
+        if(SerialWatch == 'b'){
+            printf("データの受信に失敗しました\n");
+        }
+    }else{
+        if (data >= 192) BallDistance = 4;
+        else if (data >= 128) BallDistance = 3;
+        else if (data >= 64) BallDistance = 2;
+        else BallDistance = 1;
+        //下位6bitはBallAnlge
+        if (data % 64 == 63) {
+            BallAngle = -999;
+        } else {
+            BallAngle = data % 64 * 11.25;
+        }
+        if (SerialWatch == 'b') {
+            printf("data : 0x%x BallDistance : %d BallAngle : %f \n",data,BallDistance,BallAngle);
+        }
+    }
+}
+
+void UseBLE(){
+    int kurikaesi = 0;
+    while (!uart_is_writable(uart1)) {  
+        kurikaesi++;
+        if(kurikaesi > 100){
+            kurikaesi = 0;
+            uart_flush();
+            if(SerialWatch == 'B'){
+                printf("flushしました。\n");
+            }
+        }
+    }
+    //BallSensorの値を要求
+    if(mode == 1 || mode == 2 || mode == 99){
+        uart_putc(uart1,0x02); 
+    }else{
+        uart_putc(uart1,0x03); 
+    }
+    uint8_t data = uart_getc_timeout(uart1, 10); 
+    if(data == PICO_ERROR_TIMEOUT){
         BallAngle = -999;
         if(SerialWatch == 'b'){
             printf("データの受信に失敗しました\n");
